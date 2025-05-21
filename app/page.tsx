cc1cf39259ef74b46ca2c45262963e8b62c00012
Kartik Agarwal <kartik_a@mt.iitr.ac.in>
2025-05-22 04:22:40 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-'use client'
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-
 export default function Home() {
-  const [activeTab, setActiveTab] = useState<"claims" | "denied">("claims");
-
   return (
-   <>ugu</>
+   <></>
   );
 }
